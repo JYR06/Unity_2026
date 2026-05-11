fd4b57fb1d40334b5ef1ef1fa6af68748b248068
JYR06 <yourang4631@gmail.com>
2026-05-11 16:05:27 +0900

    05/11 수업 실습

--- Ch07/Assets/2. Scripts/TargetController.cs
+++ Ch07/Assets/2. Scripts/TargetController.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class TargetController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    GameObject player;
+
+    private void Start()
     {
-        
+        player = GameObject.Find("Player");
+    }
+    private void Update()
+    {
+        transform.LookAt(player.transform);   
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
